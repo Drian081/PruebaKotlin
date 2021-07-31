91179b636e4c94530f7dec5abbcd5f389a0ddd42
ADRIAN MARTINEZ MARTINEZ <87771017+Drian081@users.noreply.github.com>
2021-07-30 22:10:34 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,47 @@
+package com.adrian081.pruebakotlin
+
+//TODO: impementar metodo main
+//crear funcion: fun + *identificador + *parametros* o parentisis
+
+fun main () {
+    val numero1 = 1
+    val numero2: Int = 10
+
+
+    if (numero1 > numero2) {
+        println("numero 1 es mayor ")
+    } else {
+        println("no se cumplio")
+    }
+    println("Termino mi programa")
+}
+/*
+val numero1= 5
+val numero2 : Int
+numero2 = 10
+forma no comun o no recomedada pero si es valida
+val numero3 : Int=10
+println(numero1)
+print ("Bienvenidos")
+println(2)
+print(false)
+float // string // etc para no inicializar como el ejemplo de arriba
+fun print(){}
+*/
+
+/** practica Moviles 2
+ * TODO: Terminar documentacion
+ * paso 1: Crear la funcion main
+ * paso 2: Aprender S¡ntaxis General
+ **/
+
+
+//TODO: impementar metodo main @Fatima
+
+//hay dos maneras de implementar metodo main
+//como crear una variable val+*identificador*
+//iniciar variable: *identificador* + = *valor?*
+
+// crear funcion: fun + *identificador + *parametros* o parentisis
+
+
